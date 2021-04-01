03231b5966afbf69af791c3844877ea84a2d9338
administrator <2316696377@qq.com>
2021-04-01 14:25:43 +0800

    整个项目使用maven管理

--- backend/module-system/src/main/java/com/fy/system/config/MapRepositoryConfig.java
+++
@@ -1,9 +0,0 @@
-package com.fy.system.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.map.repository.config.EnableMapRepositories;
-
-@Configuration
-@EnableMapRepositories
-public class MapRepositoryConfig {
-}
